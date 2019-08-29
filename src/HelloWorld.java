792aa6cdafb255ecf63f28f357f1f6f4823968ff
ewan akins <earfluffy@gmail.com>
2019-08-29 14:37:24 -0700

    Gibberish added to printed string.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args)
     {
-        System.out.println("My THIRD Hello World!");
+        System.out.println("My THIRDasldfkjkasl;df Hello World!");
     }
 }
